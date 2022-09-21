85b03978356818fa93d0ed182a68741a995f1676
unknown <2daw3@HZ114487.H012108.NET>
2022-09-21 10:41:46 +0200

    Faltan 3 y 4

---
+++ datos.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Nombre: <?php echo $_POST["nombre"]; ?><br>
+Apellidos: <?php echo $_POST["apellidos"]; ?><br>
+Edad: <?php echo $_POST["edad"]; ?><br>
+Peso: <?php echo $_POST["peso"]; ?><br>
+Sexo: <?php echo $_POST["sexo"]; ?><br>
+Estado Civil: <?php echo $_POST["estado"]; ?><br>
+Aficiones: <?php echo $_POST["aficiones"]; ?><br>
+
+</body>
+</html> 
---
+++ ejercicio1Login.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Usuario: <?php echo $_POST["nombre"]; ?><br>
+Password <?php echo $_POST["password"]; ?>
+
+</body>
+</html> 
